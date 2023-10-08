27dbdc5a35379c52fca80f33e606b713efe33918
Xkl-xx <2857093266@qq.com>
2023-10-08 11:12:41 +0800

    预计会失败的提交

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable no-console */
 import './App.css';
 import React from 'react';
 import { Button } from 'antd';
 import { getLoginQrCKey } from './api';
-
-console.log(process.env.NODE_ENV);
 
 function App() {
   const fn = () => {
