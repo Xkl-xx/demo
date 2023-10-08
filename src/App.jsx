d5b53a402ae571993353dc7b33f5ff120731ebe0
Xkl-xx <2857093266@qq.com>
2023-10-08 09:47:23 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+import { getLoginQrCKey } from './api';
+
+console.log(process.env.NODE_ENV);
+
+function App() {
+  const fn = () => {
+    getLoginQrCKey()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <Button type="primary" onClick={fn}>
+      按钮
+    </Button>
+  );
+}
+
+export default App;
